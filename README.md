6ec84e01d32cff4fe994940fe3e61638b616f65a
honeymon <ihoneymon@gmail.com>
2016-05-20 21:32:51 +0900

    build.sh 수정

--- README.md
+++ README.md
@@ -4,3 +4,13 @@
 * 작성자: 허니몬 <ihoneymon@gmail.com>
 
 > 시작은 5월 말에서 6월부터...
+
+## 문서생성을 위한 사전요구사항
+* 루비RUBY 설치되어 있어야 함
+* [bundler](http://bundler.io/) 설치되어 있어야 함 
+
+## 문서빌드
+```
+$ bundle install
+$ ./build.sh
+```
--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-#!/usr/bin/env bash
-asciidoctor index.adoc -o spring-boot-dev-guide.html
-asciidoctor-pdf -r asciidoctor-pdf-cjk-kai_gen_gothic -a pdf-style=KaiGenGothicKR index.adoc -o spring-boot-dev-guide.pdf
+#!/usr/bin/env zsh
+bundle exec rake book:build
